49f950d6db1c78d39a90b10265c5f54c9c808983
maksym <maksym.polyakov@yahoo.ca>
2024-04-02 20:56:37 -0500

    debug test

--- average.js
+++ average.js
@@ -1,10 +1,8 @@
-function average(list) {
-    var sum = 0;
-  
-    for (var num of list) {
-      sum ++ num;
-    }
-  
-    return sum / list.length;
-  
-console.log(average([3, 5, 7]));
+let average = function(list) {
+  let sum = 0;
+  for (let num of list) {
+    sum += num;
+  }
+  return sum / list.length;
+};
+console.log(average([3, 6, 1]));
--- reverse.js
+++ reverse.js
@@ -1,8 +1,7 @@
-var input = process.argv[2];
-
+let input = process.argv[2];
+let reverse = function(original) {
+  return original.split('').reverse().join('');
+};
 if (input)
   console.log(reverse(input));
 
-function reverse(original) {
-  return orignal.split('').reverseList().join('');
-}
